f32c0203fa32f909a491fc0b056130128a456df4
thaaisgonsales <thaaisgonsales@gmail.com>
2023-08-22 17:11:22 -0400

    criando o projeto para exemplo de tipos e variaveis

--- tipos-variaveis/src/TipoVariaves.java
+++ tipos-variaveis/src/TipoVariaves.java
@@ -1,2 +1,20 @@
 public class TipoVariaves {
+
+public static void main(String[] args){
+
+
+    double salarioMinimo = 2500.33;
+
+    short numeroCurto = 1;
+    int numeroNormal = numeroCurto;
+    short numeroCurto2 = (short) numeroNormal;
+
+    int numero = 5;
+    numero = 10;
+
+    final double VALOR_PI = 20;
+
+    System.out.println(VALOR_PI);
+}
+
 }
