5c864cd4a4d1b0302b1119af124933906718ce7e
thaaisgonsales <thaaisgonsales@gmail.com>
2023-08-23 15:17:43 -0400

    criando o projeto para exemplo de tipos de operadores

--- operadores/src/operadores_java.java
+++ operadores/src/operadores_java.java
@@ -1,2 +1,34 @@
 public class operadores_java {
+
+    public static void main(String[] args) {
+        //Multiplicacao
+
+        int numero1 = 50;
+        int numero2 = 100;
+        int total = numero1 * numero2;
+
+        System.out.println(total);
+
+        //Subtracao
+
+       int numero3 = 50;
+       int numero4 = 20;
+       int total2 = numero3 - numero4;
+
+       System.out.println(total2);
+
+        //Soma
+
+        double numero5 = 10.2;
+        double numero6 = 15.5;
+        double total3 = numero5 + numero6;
+
+        System.out.println(total3);
+
+        //Modulo
+
+        int modulo = 18 % 3;
+        System.out.println(modulo);
+
+    }
 }
